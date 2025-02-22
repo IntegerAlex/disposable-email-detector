ae592bb7ee8908ab5eea7248a232c46fdd2dbdb5
Ryan Guarasica <rguarascia@gmail.com>
2025-02-22 11:26:51 -0500

    add github + external sources feature

--- index.ts
+++ index.ts
@@ -11,11 +11,48 @@ const loadDomains = async () => {
   return disposableDomains;
 }
 
+export class ExternalSourceError extends Error {
+  constructor(message: string = 'URL must be provided when loading from external source') {
+    super(message);
+    this.name = 'ExternalSourceError';
+    Object.setPrototypeOf(this, ExternalSourceError.prototype);
+  }
+}
+
+const loadDomainsFromExternalSource = async ({github, url}: LoadFromSource) => {
+    if (cachedDomains) return cachedDomains;
+    const URL = github ? "https://raw.githubusercontent.com/IntegerAlex/disposable-email-detector/refs/heads/main/index.json" : url;
+    if (!URL) throw new ExternalSourceError();
+    try {
+        const response = await fetch(URL);
+        const disposableDomains = await response.json();
+        cachedDomains = disposableDomains
+        return disposableDomains;
+    } catch (error) {
+        throw new ExternalSourceError("Failed to load disposable domains from the provided URL");
+    }
+}
+
+type LoadFromSource = {
+    github?: boolean;
+    url?: string;
+}
+
+type Options = {
+    loadFromSource?: LoadFromSource;
+}
+
 // Function to detect disposable email addresses
-export default async function disposableEmailDetector(email: string): Promise<boolean> {
+export default async function disposableEmailDetector(email: string, options?: Options): Promise<boolean> {
   try {
-    // Load the list of disposable email domains from the index.json file
-    const disposableDomains = await loadDomains();
+      let disposableDomains = [];
+    // Load from external source
+    if (options?.loadFromSource) {
+        disposableDomains = await loadDomainsFromExternalSource(options.loadFromSource);
+    } else {
+        disposableDomains = await loadDomains();
+    }
+    
     
     // Extract the domain from the email address
     const domain = email.split('@')[1].toLowerCase(); // Get the domain part of the email address and convert it to lowercase
@@ -28,7 +65,10 @@ export default async function disposableEmailDetector(email: string): Promise<bo
       console.error('index.json not found. Please create it with disposable domains.');
     } else if (error instanceof SyntaxError) {
       console.error('Invalid JSON format in index.json. Please correct the file.');
-    } else {
+    } else if (error instanceof ExternalSourceError) {
+        console.error(error.message);
+    }
+    else {
       console.error('Unexpected error:', error);
     }
     return false; 
